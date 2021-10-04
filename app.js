9a99cd0c616a0a5afb323ef8a083b60ebdb267fb
Jahid Mirza <58945047+Jahidmirza@users.noreply.github.com>
2021-10-04 11:59:12 +0600

    updated

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-# Calculator
+function Claer() {
+   document.querySelector(".input").value = "";
+}
+
+function del() { 
+   const outputDel = document.querySelector(".input");
+   outputDel.value = outputDel.value.slice(0, -1);
+}
+
+function display(newValue) {
+   document.querySelector(".input").value += newValue;
+}
+
+function answer() {
+   const a = document.querySelector(".input").value;
+   const b = eval(a);
+   document.querySelector(".input").value = b ;
+}
